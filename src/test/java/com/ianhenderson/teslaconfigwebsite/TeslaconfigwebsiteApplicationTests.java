3b8128887d4bc60090eb49e5dcbf739f376f90f8
Ian Henderson <ian.henderson1215@gmail.com>
2023-04-24 21:53:38 -0700

    first

---
+++ src/test/java/com/ianhenderson/teslaconfigwebsite/TeslaconfigwebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ianhenderson.teslaconfigwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeslaconfigwebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
